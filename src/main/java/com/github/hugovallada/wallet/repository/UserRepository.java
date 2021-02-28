43c0f8bfd5c824eac3948cc3e42957e153333797
hugovallada <valladahugo@gmail.com>
2021-02-28 14:37:42 -0300

    Cria entidade , repositório e o teste de busca por email

--- src/main/java/com/github/hugovallada/wallet/repository/UserRepository.java
+++ src/main/java/com/github/hugovallada/wallet/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.github.hugovallada.wallet.repository;
 import com.github.hugovallada.wallet.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepositoory extends JpaRepository<User, Long> {
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
 }
