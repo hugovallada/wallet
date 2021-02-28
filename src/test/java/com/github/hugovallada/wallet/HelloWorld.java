99bee54ebf9446480677cdbdea1840a05bbf042b
hugovallada <valladahugo@gmail.com>
2021-02-28 12:53:19 -0300

    Configura o ambiente para testes

--- src/test/java/com/github/hugovallada/wallet/HelloWorld.java
+++ src/test/java/com/github/hugovallada/wallet/HelloWorld.java
@@ -1,15 +1,15 @@
 package com.github.hugovallada.wallet;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
-class WalletApplicationTests {
+public class HelloWorld {
 
     @Test
-    void contextLoads() {
+    public void testHelloWorld() {
+        Assertions.assertEquals(1, 1);
     }
 
 }
