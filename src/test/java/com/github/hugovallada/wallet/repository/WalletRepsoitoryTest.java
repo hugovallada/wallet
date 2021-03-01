609d657b4e25bbe8cf0c0a7386b9a94fd47a3a83
hugovallada <valladahugo@gmail.com>
2021-03-01 19:19:35 -0300

    Cria repositório wallet e teste de repositório wallet save

---
+++ src/test/java/com/github/hugovallada/wallet/repository/WalletRepsoitoryTest.java
@@ -0,0 +1,28 @@
+package com.github.hugovallada.wallet.repository;
+
+import com.github.hugovallada.wallet.entity.Wallet;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+
+@SpringBootTest
+public class WalletRepsoitoryTest {
+    private static final String NAME = "walletTest";
+
+    @Autowired
+    WalletRepository repository;
+
+    @Test
+    public void testSave() {
+        Wallet wallet = new Wallet();
+        wallet.setName(NAME);
+        wallet.setValue(BigDecimal.valueOf(100.90));
+
+        Wallet savedWallet = repository.save(wallet);
+
+        Assertions.assertNotNull(savedWallet);
+    }
+}
